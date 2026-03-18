49ad57b029e05433fbb6b00b3516b52f5a82551f
Joseph N <nietj@farmingdale.edu>
2026-03-17 21:10:59 -0400

    Added inital project structure for this awesome group(Fixed it)

--- backend-api/src/main/java/com/smartpark/backendapi/BackendApiApplication.java
+++ backend-api/src/main/java/com/smartpark/backendapi/BackendApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendApiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BackendApiApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApiApplication.class, args);
+	}
 
 }
