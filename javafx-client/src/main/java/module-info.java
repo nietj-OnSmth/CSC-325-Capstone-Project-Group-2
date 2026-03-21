b08b3fb47eadc62d12de81259e5eca666f7bc3e1
kaleysantonocito <kaleysantonocito31@gmail.com>
2026-03-20 20:32:30 -0400

    Remove default HelloController.java

--- javafx-client/src/main/java/module-info.java
+++ javafx-client/src/main/java/module-info.java
@@ -6,4 +6,8 @@ module com.smartpark.javafxclient {
 
     opens com.smartpark.javafxclient to javafx.fxml;
     exports com.smartpark.javafxclient;
+    exports ui;
+    opens ui to javafx.fxml;
+    exports com.smartpark.javafxclient.ui;
+    opens com.smartpark.javafxclient.ui to javafx.fxml;
 }
