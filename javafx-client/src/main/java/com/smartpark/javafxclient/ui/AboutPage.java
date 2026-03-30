e27a18a27b1566fa9b89b03392db7bd59031c2d6
kaleysantonocito <kaleysantonocito31@gmail.com>
2026-03-30 19:09:29 -0400

    designed about and help pages

--- javafx-client/src/main/java/com/smartpark/javafxclient/ui/AboutPage.java
+++ javafx-client/src/main/java/com/smartpark/javafxclient/ui/AboutPage.java
@@ -1,4 +1,59 @@
 package com.smartpark.javafxclient.ui;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
 public class AboutPage {
+
+    public VBox getView() {
+        VBox root = new VBox(20);
+        root.setPadding(new Insets(40));
+        root.setAlignment(Pos.TOP_CENTER);
+        root.setStyle("-fx-background-color: #F5F7F6;");
+
+        Label title = new Label("About SmartPark");
+        title.setStyle(
+                "-fx-font-size: 32px;" +
+                        "-fx-font-family: 'Helvetica';" +
+                        "-fx-font-weight: bold;" +
+                        "-fx-text-fill: #0B5E3C;"
+        );
+
+        Label subtitle = new Label("A smarter way to find campus parking");
+        subtitle.setStyle(
+                "-fx-font-size: 18px;" +
+                        "-fx-font-family: 'Arial';" +
+                        "-fx-text-fill: #555555;"
+        );
+
+        VBox contentBox = new VBox(15);
+        contentBox.setMaxWidth(750);
+        contentBox.setPadding(new Insets(30));
+        contentBox.setStyle(
+                "-fx-background-color: white;" +
+                        "-fx-background-radius: 14;" +
+                        "-fx-border-color: #D9E2DD;" +
+                        "-fx-border-radius: 14;"
+        );
+
+        Label paragraph1 = new Label(
+                "SmartPark is a campus parking application designed to help students, faculty, and visitors find available parking more efficiently. The system is intended to improve the parking experience by providing users with clear and accessible parking information."
+        );
+        paragraph1.setWrapText(true);
+        paragraph1.setStyle(
+                "-fx-font-size: 15px;" +
+                        "-fx-font-family: 'Arial';" +
+                        "-fx-text-fill: #333333;"
+        );
+
+
+
+        contentBox.getChildren().addAll(paragraph1);
+
+        root.getChildren().addAll(title, subtitle, contentBox);
+
+        return root;
+    }
 }
--- javafx-client/src/main/java/module-info.java
+++ javafx-client/src/main/java/module-info.java
@@ -6,8 +6,6 @@ module com.smartpark.javafxclient {
 
     opens com.smartpark.javafxclient to javafx.fxml;
     exports com.smartpark.javafxclient;
-    exports ui;
-    opens ui to javafx.fxml;
     exports com.smartpark.javafxclient.ui;
     opens com.smartpark.javafxclient.ui to javafx.fxml;
 }
