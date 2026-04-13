8f713f49cca6e10669399be8c9ecc304bb78b329
kaleysantonocito <kaleysantonocito31@gmail.com>
2026-04-13 17:25:53 -0400

    renamed parking page

--- javafx-client/src/main/java/com/smartpark/javafxclient/ui/ParkingResultsPage.java
+++ javafx-client/src/main/java/com/smartpark/javafxclient/ui/ParkingResultsPage.java
@@ -6,7 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
-public class ParkingPage {
+public class ParkingResultsPage {
 
     public VBox getView() {
         VBox root = new VBox(20);
