0706e6aa393a07adc514c24b8143c80a9552b8f1
kaleysantonocito <kaleysantonocito31@gmail.com>
2026-04-24 18:28:54 -0400

    changed font size

--- javafx-client/src/main/java/com/smartpark/javafxclient/ui/StudentDashboardPage.java
+++ javafx-client/src/main/java/com/smartpark/javafxclient/ui/StudentDashboardPage.java
@@ -102,16 +102,16 @@ public class StudentDashboardPage {
         Label welcome = new Label("Welcome, Student");
         welcome.setStyle(
                 "fx-text-fill: white;" +
-                "-fx-font-size: 30px;" +
-                        "-fx-font-family: 'Helvetica';" +
-                        "-fx-font-weight: bold;"
+                "-fx-font-size: 34px;" +
+                "-fx-font-family: 'Helvetica';" +
+                "-fx-font-weight: bold;"
         );
 
         Label role = new Label("Logged in as: Student");
         role.setStyle(
                 "fx-text-fill: white;" +
-                "-fx-font-size: 16px;" +
-                        "-fx-font-family: 'Helvetica';"
+                "-fx-font-size: 18px;" +
+                "-fx-font-family: 'Helvetica';"
         );
 
         Button findParking = new Button("Find Parking");
